deba90484b972ab92a8015119510ca84b00eaffc
VirginieGodfrin <virginiegodfrin@gmail.com>
2021-10-27 07:37:46 +0200

    88. Passing Data with Event and Property Binding

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,5 @@
 import { isLoweredSymbol } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,27 +8,32 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   // An array of Recipes Objects
   recipes: Recipe[] = [
     new Recipe(
       'Tarte au pomme',
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'La tarte au pomme lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
     ),
     new Recipe(
-      'Tarte au pomme',
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Tarte au poire',
+      'La tarte au poire lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
     ),
     new Recipe(
-      'Tarte au pomme',
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Tarte au prune',
+      'La tarte au prune lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
     )
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
