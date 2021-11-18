1c4bee301702afc72e23223d0b2467f515a87e1a
VirginieGodfrin <virginiegodfrin@gmail.com>
2021-11-18 07:16:50 +0100

    122. Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -7,17 +8,29 @@ export class RecipeService {
       new Recipe(
         'Tarte au pomme',
         'La tarte au pomme lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg',
+        [
+          new Ingredient('milk', 1),
+          new Ingredient('apple', 10)
+        ]
       ),
       new Recipe(
         'Tarte au poire',
         'La tarte au poire lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg',
+        [
+          new Ingredient('milk', 1),
+          new Ingredient('poire', 10)
+        ]
       ),
       new Recipe(
         'Tarte au prune',
         'La tarte au prune lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg',
+        [
+          new Ingredient('milk', 1),
+          new Ingredient('prune', 10)
+        ]
       )
   ];
 
